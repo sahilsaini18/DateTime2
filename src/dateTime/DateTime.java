db51086f715e44a6b99f5359b7295ee072335150
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:37:17 +0530

    DateTime.java

--- src/dateTime/DateTime.java
+++ src/dateTime/DateTime.java
@@ -1,36 +1,35 @@
 package dateTime;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+import java.util.logging.Logger;
+
 /**
- * Firstly i have taken input string 
- * DateTimeFormatter is used to format the time
- * Then the user string is parsed and operation is performed
+ * Firstly, I have taken input string DateTimeFormatter is used to format the
+ * time Then the user string is parsed, and operations are performed
  */
 public class DateTime {
+	private static final Logger log = Logger.getLogger(DateTime.class.getName());
+
 	public static void main(String[] args) {
-        
-		 Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 
-	        System.out.print("Enter date and time (dd/MM/yyyy HH:mm): ");
-	        String userInput = scanner.nextLine();
+		log.info("Enter date and time (dd/MM/yyyy HH:mm): ");
+		String userInput = scanner.nextLine();
 
-	        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
-	        LocalDateTime inputDateTime = LocalDateTime.parse(userInput, formatter);
+		LocalDateTime inputDateTime = LocalDateTime.parse(userInput, formatter);
 
-	        // Perform operations
-	        LocalDateTime plusOneDay = inputDateTime.plusDays(1);
-	        LocalDateTime minusTenDays = inputDateTime.minusDays(10);
+		// Perform operations
+		LocalDateTime plusOneDay = inputDateTime.plusDays(1);
+		LocalDateTime minusTenDays = inputDateTime.minusDays(10);
 
-	 
-	        System.out.println("Original Date and Time: " + inputDateTime.format(formatter));
-	        System.out.println("Date and Time after adding one day: " + plusOneDay.format(formatter));
-	        System.out.println("Date and Time after subtracting 10 days: " + minusTenDays.format(formatter));
+		log.info("Original Date and Time: " + inputDateTime.format(formatter));
+		log.info("Date and Time after adding one day: " + plusOneDay.format(formatter));
+		log.info("Date and Time after subtracting 10 days: " + minusTenDays.format(formatter));
 
-	        scanner.close();
+		scanner.close();
 	}
-
-
-
 }
